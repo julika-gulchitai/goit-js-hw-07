c2f86c9bd34e41d744ec919abc64baa50725fe2f
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-11-05 01:35:25 +0200

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,32 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const instance = basicLightbox.create(`
-    <img class="modal" src="" width="800" height="600">
-`)
+    <img class="modal" src="" width="800" height="600">`)
 
-
-const gallery = galleryItems.map(({ preview, original, description }) => { 
-    return `<li class="gallery__item">
-  <a class="gallery__link" href="">
+const gallery = document.querySelector('ul');
+let galleryElements = document.createElement('li');
+galleryElements = galleryItems.map((picture) => 
+    `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
-      src="${preview}"
-        data-source="${original}"
-      alt="${description}"
+      src="${picture.preview}"
+      data-source="${picture.original}"
+      alt="${picture.description}"
     />
   </a>
-</li>`
-})
-const urlItem = document.querySelector('.gallery');
-urlItem.insertAdjacentHTML('afterbegin', gallery.join(''));
+</li>`).join('');
+gallery.innerHTML = galleryElements;
 
-urlItem.addEventListener('click', (event) => {
+gallery.addEventListener('click', (event) => {
     if (event.target.tagName === 'IMG') {
         event.preventDefault();
         instance.show();
         document.querySelector('.modal').src = event.target.dataset.source;  
-}
+    }
 })
 
-
+document.addEventListener('keydown', (event) => {
+    if (event.target.tagName === 'Escape') 
+        document.querySelector('.modal').src = event.target.dataset.source;
+        instance.close();
+});
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('ul');
+let galleryElements = document.createElement('li');
+
+galleryElements = galleryItems.map((picture) => 
+  `<li class="gallery__item">
+   <a class="gallery__link" href="${picture.original}">
+      <img
+      class="gallery__image"
+      src="${picture.preview}"
+      alt="${picture.description}" />
+   </a>
+</li>`).join('');
+gallery.innerHTML = galleryElements;
+
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
 console.log(galleryItems);
